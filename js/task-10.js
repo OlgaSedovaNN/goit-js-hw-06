1fcad028f61e20ccdffb3dcba79cc4ad1d3d36f6
OlgaSedovaNN <olga_04021991@mail.ru>
2022-01-11 17:34:54 +0300

    6дз + корявая 10задача

--- js/task-10.js
+++ js/task-10.js
@@ -7,40 +7,63 @@ const input = document.querySelector('input');
 btnCreate.addEventListener('click', createBoxes)
 
 function createBoxes(amount) {
-amount = input.value
-amount = Number(amount)
+  amount = input.value
+  amount = Number(amount)
 
-   let size = 20;
- 
-  for (let i = 0; i < amount; i += 1) {
-    const newDiv = document.createElement('div')
+  
+  if (!divBoxes.querySelector('.forRemove')) {
+    let size = 20;
+    for (let i = 0; i < amount; i += 1) {
+      const newDiv = document.createElement('div')
  
 
-    function getRandomHexColor() {
-     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
-    newDiv.style.backgroundColor = getRandomHexColor();
+      function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      }
+      newDiv.style.backgroundColor = getRandomHexColor();
    
-    size += 10;
-     newDiv.style.width = `${size}px`;
-    newDiv.style.height = `${size}px`;
+      size += 10;
+      newDiv.style.width = `${size}px`;
+      newDiv.style.height = `${size}px`;
   
     
-    newDiv.classList.add('forRemove')
-    divBoxes.appendChild(newDiv)
+      newDiv.classList.add('forRemove')
+      divBoxes.appendChild(newDiv)
+    
+    }
+
+  } else {
+    
+    let lastElSize = divBoxes.lastChild.style.width;
+    lastElSize = Number.parseInt(lastElSize)
+
+    for (let i = 0; i < amount; i += 1) {
+      const newDiv = document.createElement('div')
+ 
+
+      function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      }
+      newDiv.style.backgroundColor = getRandomHexColor();
+   
+      lastElSize += 10;
+      newDiv.style.width = `${lastElSize}px`;
+      newDiv.style.height = `${lastElSize}px`;
+  
+    
+      newDiv.classList.add('forRemove')
+      divBoxes.appendChild(newDiv)
+    }
     
   }
-
-} 
-
+}
 
 btnDestroy.addEventListener('click', destroyBoxes)
 
-function destroyBoxes() { 
-  const removeDivs = document.querySelectorAll('.forRemove')
-  for (let i = 0; i < removeDivs.length; i += 1) {
-    removeDivs[i].remove();
+  function destroyBoxes() {
+    const removeDivs = document.querySelectorAll('.forRemove')
+    for (let i = 0; i < removeDivs.length; i += 1) {
+      removeDivs[i].remove();
+    }
+    input.value = '';
   }
-  input.value = '';
-  
-}
